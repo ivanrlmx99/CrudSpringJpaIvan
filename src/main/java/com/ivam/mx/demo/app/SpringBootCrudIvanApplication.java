efa069e02008671d0d7c292e72429d9b7cf6f2a1
ivanrlmx99 <ivanlaureano77@gmail.com>
2020-06-12 22:35:08 -0500

    se agego plantilla

---
+++ src/main/java/com/ivam/mx/demo/app/SpringBootCrudIvanApplication.java
@@ -0,0 +1,13 @@
+package com.ivam.mx.demo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudIvanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCrudIvanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ivam/mx/demo/app/SpringBootCrudIvanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivam.mx.demo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudIvanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
